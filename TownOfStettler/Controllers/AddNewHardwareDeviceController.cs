34a992688dbd8459e0d4f6e16c4ca8e025e49add
Grant Stensrud <stensrud@ualberta.ca>
2022-10-27 12:18:45 -0600

    Push for JM to merge;

--- TownOfStettler/Controllers/AddNewHardwareDeviceController.cs
+++ TownOfStettler/Controllers/AddNewHardwareDeviceController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TownOfStettler.Data;
+using TownOfStettler.Models;
+using TownOfStettler.Models.Exceptions;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using MessagePack;
+using ValidationException = TownOfStettler.Models.Exceptions.ValidationException;
 
 namespace TownOfStettler.Controllers
 {
     public class AddNewHardwareDeviceController : Controller
     {
+        private readonly DatabaseContext _context;
+        public AddNewHardwareDeviceController(DatabaseContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            // If there are exceptions, store them in the view data/bag
+            if (TempData["Exceptions"] != null)
+            {
+                ViewData["Exceptions"] =
+                     JsonConvert.DeserializeObject(TempData["Exceptions"].ToString(), typeof(ValidationException));
+            }
+
             return View();
+        }
+
+        public IActionResult EnterNewHardwareDevice()
+        {
+
         }
     }
 }
